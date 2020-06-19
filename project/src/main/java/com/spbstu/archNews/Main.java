46235c3dd4b6f9f787a567b00f027aea99e8a885
Kislichenko Bogdan <bogdan600@yandex.ru>
2020-06-19 14:20:16 +0300

    add simple gui

--- project/src/main/java/com/spbstu/archNews/Main.java
+++ project/src/main/java/com/spbstu/archNews/Main.java
@@ -3,13 +3,26 @@ package com.spbstu.archNews;
 
 import com.spbstu.archNews.models.Person;
 import com.spbstu.archNews.services.PersonService;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
-        PersonService userService = new PersonService();
-        Person user = new Person("login1", "Bogdan", "bogdan@archnews.ru");
-        userService.saveUser(user);
-        userService.updateUser(user);
+        launch(args);
+//        PersonService userService = new PersonService();
+//        Person user = new Person("login1", "Bogdan", "bogdan@archnews.ru");
+//        userService.saveUser(user);
+//        userService.updateUser(user);
 
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/auth.fxml"));
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
